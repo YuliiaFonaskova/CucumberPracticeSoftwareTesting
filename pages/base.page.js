246f36cd315da7d68ca408259ca2a766e9334cc7
YuliiaFonaskova <yuliia.fonaskova@gmail.com>
2026-07-23 22:14:04 +0200

    Remove temporary CI diagnostics

--- pages/base.page.js
+++ pages/base.page.js
@@ -1,19 +1,6 @@
-const fs = require("fs-extra");
-
 class BasePage {
   async open(path) {
     await browser.url(path);
-
-    const currentUrl = await browser.getUrl();
-    const title = await browser.getTitle();
-
-    console.log("OPENED PATH:", path);
-    console.log("CURRENT URL:", currentUrl);
-    console.log("PAGE TITLE:", title);
-
-    fs.ensureDirSync("./reports/screenshots");
-
-    await browser.saveScreenshot(`./reports/screenshots/${Date.now()}-page.png`);
   }
 }
 
